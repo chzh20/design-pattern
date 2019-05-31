6563803f3e380924793e792c51428b9dd43358b4
Sim-Chen <chenzhendoit@163.com>
2019-05-31 13:14:57 +0800

    examples

---
+++ 设计模式/06_装饰模式/decorator3.cpp
@@ -0,0 +1,123 @@
+//业务操作
+class Stream{
+
+public：
+    virtual char Read(int number)=0;
+    virtual void Seek(int position)=0;
+    virtual void Write(char data)=0;
+    
+    virtual ~Stream(){}
+};
+
+//主体类
+class FileStream: public Stream{
+public:
+    virtual char Read(int number){
+        //读文件流
+    }
+    virtual void Seek(int position){
+        //定位文件流
+    }
+    virtual void Write(char data){
+        //写文件流
+    }
+
+};
+
+class NetworkStream :public Stream{
+public:
+    virtual char Read(int number){
+        //读网络流
+    }
+    virtual void Seek(int position){
+        //定位网络流
+    }
+    virtual void Write(char data){
+        //写网络流
+    }
+    
+};
+
+class MemoryStream :public Stream{
+public:
+    virtual char Read(int number){
+        //读内存流
+    }
+    virtual void Seek(int position){
+        //定位内存流
+    }
+    virtual void Write(char data){
+        //写内存流
+    }
+    
+};
+
+//扩展操作
+
+DecoratorStream: public Stream{
+protected:
+    Stream* stream;//...核心 用组合的方式引入多态，消除不良继承-->
+    //既继承又组合
+    // 继承接口，组合是为了实现了某个具体对象的功能扩展，减少代码量
+    DecoratorStream(Stream * stm):stream(stm){
+    
+    }
+    
+};
+
+class CryptoStream: public DecoratorStream {
+ 
+
+public:
+    CryptoStream(Stream* stm):DecoratorStream(stm){
+    
+    }
+    
+    
+    virtual char Read(int number){
+       
+        //额外的加密操作...
+        stream->Read(number);//读文件流
+    }
+    virtual void Seek(int position){
+        //额外的加密操作...
+        stream::Seek(position);//定位文件流
+        //额外的加密操作...
+    }
+    virtual void Write(byte data){
+        //额外的加密操作...
+        stream::Write(data);//写文件流
+        //额外的加密操作...
+    }
+};
+
+
+
+class BufferedStream : public DecoratorStream{
+    
+    Stream* stream;//...
+    
+public:
+    BufferedStream(Stream* stm):DecoratorStream(stm){
+        
+    }
+    //...
+};
+
+
+
+
+void Process(){
+
+    //运行时装配
+    FileStream* s1=new FileStream();
+    
+    CryptoStream* s2=new CryptoStream(s1);
+    
+    BufferedStream* s3=new BufferedStream(s1);
+    
+    BufferedStream* s4=new BufferedStream(s2);
+    
+    
+
+}
